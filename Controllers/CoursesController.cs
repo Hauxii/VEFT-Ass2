53b6c6f5949c4db59d0f4445e04f312a83758be1
Hauxii <haukur91@gmail.com>
2016-08-30 12:04:41 +0000

    coursescontroller template

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -8,6 +8,36 @@ namespace WebApplication.Controllers
     [Route("/api/courses")]
     public class CoursesController : Controller
     {
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok();
+        }    
+
+        [HttpPost]
+        public IActionResult AddCourse()
+        {
+            return StatusCode(201);
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name = "GetCourseByID")]
+        public IActionResult GetCourseByID()
+        {
+            return Ok();
+        }
         
+        [HttpPut]
+        [Route("{id:int}")]
+        public IActionResult EditCourse()
+        {
+            return Ok();
+        }
+
+        
+    
+    
     }
+
+
 }
